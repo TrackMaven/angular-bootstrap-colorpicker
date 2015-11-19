84047742407ca742fbe9eae5c787b9378ff732d5
Farhan Syed <nearhan@gmail.com>
2015-11-19 15:39:25 -0500

    stuff

--- js/bootstrap-colorpicker-module.js
+++ js/bootstrap-colorpicker-module.js
@@ -356,6 +356,8 @@ angular.module('colorpicker.module', [])
               colorpickerPreview = colorpickerTemplate.find('colorpicker-preview'),
               pickerColorPointers = colorpickerTemplate.find('i');
 
+          console.log('this is the value of the dash', dashed);
+
           $compile(colorpickerTemplate)($scope);
 
           if (withInput) {
