e43a4b8fcdb87e83bda8fc35ef575bef089087af
Farhan Syed <nearhan@gmail.com>
2015-11-23 15:26:45 -0500

    upped number

--- js/bootstrap-colorpicker-module.js
+++ js/bootstrap-colorpicker-module.js
@@ -547,7 +547,7 @@ angular.module('colorpicker.module', [])
             } else if (position === 'right') {
               positionValue = {
                 'top': positionOffset.top - 95,
-                'left': positionOffset.left + 30
+                'left': positionOffset.left + 32
               };
             } else if (position === 'bottom') {
               positionValue = {
